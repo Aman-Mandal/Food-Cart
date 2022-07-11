797e86fbad6821a186aa4562ba23e6cec6ac9ae6
Aman-Mandal <002amankumar@gmail.com>
2022-07-11 17:57:35 +0530

    useRef hook in forms

--- src/Components/Cart/Checkout.js
+++ src/Components/Cart/Checkout.js
@@ -1,31 +1,46 @@
+import { useRef } from 'react'
 import styles from './Checkout.module.css'
 
 const Checkout = props => {
+  const nameInputRef = useRef()
+  const streetInputRef = useRef()
+  const postalInputRef = useRef()
+  const cityInputRef = useRef()
+
   const submitFormHandler = event => {
     event.preventDefault()
+
+    const enteredName = nameInputRef.current.value
+    const enteredStreet = streetInputRef.current.value
+    const enteredPostalCode = postalInputRef.current.value
+    const enteredCity = cityInputRef.current.value
   }
   return (
-    <form onSubmit={submitFormHandler}>
+    <form className={styles.form} onSubmit={submitFormHandler}>
       <div className={styles.control}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street" />
+        <input type="text" id="street" ref={streetInputRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" />
+        <input type="text" id="postal" ref={postalInputRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityInputRef} />
       </div>
-      <button type="button" onClick={props.onCancel}>
-        Cancel
-      </button>
-      <button type="submit">Submit</button>
+      <div className={styles.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button type="submit" className={styles.submit}>
+          Submit
+        </button>
+      </div>
     </form>
   )
 }
